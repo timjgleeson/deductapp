8ab4d6a6ad06a861c65a77ed2f95890e050d07ac
Tim Gleeson <timjgleeson@gmail.com>
2012-05-11 21:16:43 +1000

    removed date from deduction

--- app/models/deduction.rb
+++ app/models/deduction.rb
@@ -1,9 +1,8 @@
 class Deduction < ActiveRecord::Base
-  attr_accessible :amount, :budget_id, :date, :description, :name, :tag_list
+  attr_accessible :amount, :budget_id, :description, :name, :tag_list
   
   validates :name, :presence => true
   validates :amount, :presence => true
-  validates :date, :presence => true
   validates :user_id, :presence => true
 
   validates :amount, :numericality => true
