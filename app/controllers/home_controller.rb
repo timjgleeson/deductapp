1835f8fc4506934f3bbfde6e626a097fb71cb01c
Tim Gleeson <timjgleeson@gmail.com>
2012-05-11 20:26:31 +1000

    continual update to layouts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,8 +8,8 @@ class HomeController < ApplicationController
     @week_end = @week_start + 1.week
 
     @deductions = current_user.deductions
-    @this_weeks_deductions = current_user.deductions.paginate(:page => params[:page], :per_page => 10).where('date between ? and ?', @week_start, @week_end).order("date DESC, budget_id ASC")
-    @latest_deductions = current_user.deductions.limit(10).order("date DESC, budget_id ASC")
+    @this_weeks_deductions = current_user.deductions.paginate(:page => params[:page], :per_page => 10).where('created_at between ? and ?', @week_start, @week_end).order("created_at DESC, budget_id ASC")
+    @latest_deductions = current_user.deductions.limit(10).order("created_at DESC, budget_id ASC")
 
     respond_to do |format|
       format.html # home.html.erb
