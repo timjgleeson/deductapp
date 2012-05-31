93813ff2a7dabfacd8ed9d940fdf91ebc9c80137
Tim Gleeson <timjgleeson@gmail.com>
2012-06-01 07:01:06 +1000

    Update to stuff

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -1,14 +1,26 @@
 class Favourite < ActiveRecord::Base
-  attr_accessible :amount, :budget_id, :description, :name, :user_id, :tag_list
+  attr_accessible :amount, :budget_id, :description, :name, :user_id, :tag_list, :transaction_type
 
   validates :name, :presence => true
   validates :amount, :presence => true
   validates :user_id, :presence => true
+  validates :transaction_type, :presence => true
 
   validates :amount, :numericality => true
+  validates :transaction_type, :numericality => true
 
   acts_as_taggable
   
   belongs_to :budget
   belongs_to :user
+
+  public
+
+  def icon
+    if transaction_type === 1
+      'minus'
+    else
+      'plus'
+    end
+  end
 end
